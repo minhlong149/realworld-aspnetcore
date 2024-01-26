210254e9cfdebee2713e0e8e0a11863b8e0df071
Long Nguyen <86862856+minhlong149@users.noreply.github.com>
2024-01-26 21:46:31 +0700

    feat: Implement create article feature

--- src/Core/Constants/ArticleConstants.cs
+++ src/Core/Constants/ArticleConstants.cs
@@ -2,5 +2,19 @@ namespace Core.Constants;
 
 public static class ArticleConstants
 {
+    public const string TitleRequired = "Title can't be empty";
+    public const string DescriptionRequired = "Description can't be empty";
+    public const string BodyRequired = "Body can't be empty";
+    public const string AuthorRequired = "Author can't be empty";
+    
+    public const string TitleLengthExceeded = "Title is too long";
+    public const string DescriptionLengthExceeded = "Description is too long";
+    public const string BodyLengthExceeded = "Body is too long";
+    
+    public const string AuthorNotFound = "Author not found";
+    
+    public const int TitleMaxLength = 100;
     public const int SlugMaxLength = 72;
+    public const int DescriptionMaxLength = 200;
+    public const int BodyMaxLength = 10000;
 }
