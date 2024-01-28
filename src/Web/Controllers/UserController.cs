9c8237306f15717bd7e08bf6f55d5c3b5cd43596
Long Nguyen <86862856+minhlong149@users.noreply.github.com>
2024-01-28 10:24:12 +0700

    fix: Refactor UpdateUser request to follow CQRS pattern

--- src/Web/Controllers/UserController.cs
+++ src/Web/Controllers/UserController.cs
@@ -1,6 +1,6 @@
 using Application.DTOs;
-using Application.Features.Users;
 using Application.Users.Commands.Create;
+using Application.Users.Commands.Update;
 using Application.Users.Queries.GetUser;
 using Core.Services;
 
@@ -8,7 +8,7 @@ namespace Web.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
-public class UserController(ISender sender) : ControllerBase
+public class UserController(ISender sender, IUser currentUser) : ControllerBase
 {
     [HttpPost("login")]
     public async Task<ActionResult<UserDto>> Authentication(GetUserRequest request)
@@ -26,12 +26,9 @@ public class UserController(ISender sender) : ControllerBase
 
     [HttpPut]
     [Authorize]
-    public async Task<ActionResult<UserDto>> UpdateUser(
-        [FromBody] UpdateUser command,
-        [FromServices] IUser currentUser
-    )
+    public async Task<ActionResult<UserDto>> UpdateUser(UpdateUserRequest request)
     {
-        var user = await sender.Send(command with { Id = currentUser.Id });
+        var user = await sender.Send(request with { Id = currentUser.Id });
         return user;
     }
 }
